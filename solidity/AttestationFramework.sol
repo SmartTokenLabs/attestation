2592df07dfd5f7bec71193a0eaf920cf4335ec51
James Sangalli <j.l.sangalli@gmail.com>
2018-08-18 14:36:45 +0800

    add basic draft solidity contract

---
+++ solidity/AttestationFramework.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.4.17;
+pragma experimental ABIEncoderV2;
+contract AttestationFramework
+{
+    
+    address[] authorities;
+    
+    struct Attestation 
+    {
+        bytes32[] attestationSaltedMerkleTree;
+        bool valid;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        address attestor;
+        address recipient;
+        bytes32 salt;
+        bytes32 key;
+        bytes32 val;
+    }
+    
+    mapping(address => Attestation[]) records;
+    
+    constructor(address[] initialAuthorities) public
+    {
+        authorities = initialAuthorities;
+    }
+    
+    function addAuthority(address newAuthority) public
+    {
+        bool isAuthorised = AttestationFramework.isAuthorised(msg.sender);
+        require(isAuthorised);
+        authorities.push(newAuthority);
+    }
+    
+    function isAuthorised(address attestor) internal view returns(bool) 
+    {
+        for(uint i = 0; i < authorities.length; i++) 
+        {
+            if(attestor == authorities[i]) 
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function setAttestation(Attestation attestation) public 
+    {
+        require(AttestationFramework.isAuthorised(msg.sender));
+        require(attestation.attestor == msg.sender);
+        require(validateMerkle(attestation));
+        records[attestation.recipient].push(attestation);
+    }
+    
+    function revokeAttestation(Attestation attestation) public view
+    {
+        require(msg.sender == attestation.attestor);
+        attestation.valid = false;
+    }
+    
+    function validateMerkle(Attestation attestation) public pure returns(bool) 
+    {
+        bytes32 keyValHashed = keccak256(abi.encodePacked(
+            attestation.key, 
+            attestation.val, 
+            attestation.salt)
+        );
+        require(keyValHashed == attestation.attestationSaltedMerkleTree[0]);
+        address signer = ecrecover(keyValHashed, attestation.v, attestation.r, attestation.s);
+        require(signer == attestation.attestor);
+        for(uint i = 0; i < attestation.attestationSaltedMerkleTree.length - 2; i++) 
+        {
+            require( attestation.attestationSaltedMerkleTree[i + 2] == 
+            keccak256(
+                abi.encodePacked(
+                    attestation.attestationSaltedMerkleTree[i], 
+                    attestation.attestationSaltedMerkleTree[i + 1])
+                )
+            );
+            
+        }
+        return true;
+    }
+    
+        
+}
