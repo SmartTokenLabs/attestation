26ea10e058d6e0ce470b20c3e0e4a62b5374bf96
colourful-land <a@colourful.land>
2018-08-28 09:58:22 +0800

    added an example merkle path so that Sangalli has something to test against

--- ethereum/lib/requires_attestation.sol
+++ ethereum/lib/requires_attestation.sol
@@ -1,13 +1,44 @@
 pragma solidity ^0.4.17;
 pragma experimental ABIEncoderV2;
 contract AttestationFramework
+
+/* Test data */
+
+    Attestation memory country;
+    country.key = "c";    /* \63 Country */
+    country.value = "SG"; /* \53\47 Singapore */
+    salt = 0xc257274276a4e539741ca11b590b9447b26a8051;
+    merklePath  = {
+        0x5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03,
+        0xe258d248fda94c63753607f7c4494ee0fcbe92f1a76bfdac795c9d84101eb317
+    }
+
+    expected_root_hash = 0xe07c2eae216a596ad2d4b7dbff488899d651f367abea039ecb13e98c212e1a2c
+
+/*
+
+Leaf node hash:
+
+$ echo 00: 63 53 47 c2 57 27 42 76 a4 e5 39 74 1c a1 1b 59 0b 94 47 b2 6a 80 51 | xxd -r -c 32 | sha256sum
+eeab27f00a1460a4409643abbe705d0d67b8ea4b9027130e2672904f76e824da
+
+Level 1 hash:
+$ (echo 00: eeab27f00a1460a4409643abbe705d0d67b8ea4b9027130e2672904f76e824da;      echo 20: 5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03) | xxd -g 32 -c 32 -r | sha256sum
+5da0fe7145fda91d4516cb3619b3463fd9318c3475d5f578fd3d1fc189084bbe
+
+Level 2 hash:
+$ (echo 00: 5da0fe7145fda91d4516cb3619b3463fd9318c3475d5f578fd3d1fc189084bbe;      echo 20: e258d248fda94c63753607f7c4494ee0fcbe92f1a76bfdac795c9d84101eb317) | xxd -g 32 -c 32 -r | sha256sum
+e07c2eae216a596ad2d4b7dbff488899d651f367abea039ecb13e98c212e1a2c
+
+*/
+
 {
     
     address[] authorities;
     
     struct Attestation 
     {
-        bytes32[] attestationSaltedMerkleTree;
+        bytes32[] merklePath;
         bool valid;
         uint8 v;
         bytes32 r;
@@ -67,17 +98,17 @@ contract AttestationFramework
             attestation.val, 
             attestation.salt)
         );
-        require(keyValHashed == attestation.attestationSaltedMerkleTree[0]);
+        require(keyValHashed == attestation.merklePath[0]);
         require(msg.sender == attestation.recipient);
         address signer = ecrecover(keyValHashed, attestation.v, attestation.r, attestation.s);
         require(signer == attestation.attestor);
-        for(uint i = 0; i < attestation.attestationSaltedMerkleTree.length - 2; i++) 
+        for(uint i = 0; i < attestation.merklePath.length - 2; i++) 
         {
-            require( attestation.attestationSaltedMerkleTree[i + 2] == 
+            require( attestation.merklePath[i + 2] == 
             keccak256(
                 abi.encodePacked(
-                    attestation.attestationSaltedMerkleTree[i], 
-                    attestation.attestationSaltedMerkleTree[i + 1])
+                    attestation.merklePath[i], 
+                    attestation.merklePath[i + 1])
                 )
             );
             
