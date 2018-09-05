04461f4f75613a5f5dcc6f7fc4f598bc7f70fcc1
James Sangalli <j.l.sangalli@gmail.com>
2018-09-05 10:20:10 +0800

    clean up ManagedList.sol logic

--- ethereum/trustlist/ERC-Readme.md
+++ ethereum/trustlist/ERC-Readme.md
@@ -1,17 +1,24 @@
-## Draft ERC - Managing lists for trusted identifiers
+## Draft ERC - Managing lists for trusted smart contract identifiers  
 
 ### Introduction
 
-It is increasing clear that we will need a way to use identifiers in the blockchain space, so much so that there needs to be an easy way to manage issuers of identifiers and easily query them when necessary.
+It is increasing clear that we will need a way to use identifiers in the blockchain space, so much so that there needs to be an easy way for a service to query a contract like this to check if an attestation is valid by an issuers contract.
 
-### Purpose of this ERC
-
-The purpose of this ERC is to provide a coherent way to manage issuers and query them.
+This ERC proposes a way to manage lists of issuer contracts and their corresponding attestation capacity.
 
 ### What's included in this draft
 
 In this draft, we have include a basic implementation and interface as well as examples of using such a list in future.
-In recent news, the New Zealand government banned the purchase of property from foreigners, we believe this is the perfect example use case for this ERC as it can demonstrate how we can check an identifier issuer against a buy claim.
+
+### The purpose of this ERC
+
+In recent news, the New Zealand government decided to crack down on foreign property owners; to buy property in New Zealand now, you need to be a NZ/AU citizen.
+
+Cases like this are a perfect example of the need for an ERC like this, as they allow you to receive a cryptographic signature from an authority which proves you are a citizen, and maps the attesters key to a smart contract.
+
+When you go to buy the property through a smart contract, the attesters key is checked against a list manager like this, validated against the corresponding contract and checked that it's capacity is high enough to allow purchase of property.
+
+Note: this is one piece of the puzzle, it also requires ERC's like these two: (TODO LINK)
 
 ### Draft spec with examples
 
--- ethereum/trustlist/ManagedList.sol
+++ ethereum/trustlist/ManagedList.sol
@@ -2,32 +2,36 @@ pragma solidity ^0.4.1;
 pragma experimental ABIEncoderV2;
 contract ManagedListERC
 {
+  //manager is the contract steward, only he/she/it can change/remove/add lists
+  //issuer is the contract that handles verification and revocation of an attestation
+  //attestation key is the key that signs attestations, note: This is not the issuer address
 
   struct List
   {
     string name;
-    string description;
-    string capacity;
-    address[] issuers;
-    uint expiry;
+    string description; //short description of what the list entails
+    string capacity; //capacity is the specification of the keys authority
+    //e.g. school id card vs passport
+    //one has the capacity to buy alcohol, the other does not.
+    address[] issuerContracts; //all these addresses are contracts, no signing capacity
+    uint expiry; //TODO is this needed?
   }
 
    // find which list the sender is managing, then add an issuer to it
-  function addIssuer(address contractAddress) public;
+  function addIssuer(address issuerContractAddress) public;
 
   //return false if the list identified by the sender doesn't have this issuer in the list
-  function removeIssuer(address contractAddress, List listToRemoveIssuerFrom) public returns(bool);
+  function removeIssuer(address issuerContractAddress, List listToRemoveIssuerFrom) public returns(bool);
 
-    //TODO this is very unclear
   /* called by services, e.g. Kiwi Properties or James Squire */
   /* loop through all issuer's contract and execute validateKey() on
    * every one of them in the hope of getting a hit, return the
    * contract address of the first hit. Note that there is an attack
    * method for one issuer to claim to own the key of another which
    * is mitigated by later design. */
-  function getIssuerByKey(address issuer, address key_id_to_validate) public returns (address);
+   //loop through the issuers array, calling validate on the signingKeyOfAttestation
+  function getIssuerCorrespondingToAttestationKey(uint list_id, address signingKeyOfAttestation) public returns (address);
 
-    //TODO why not map to an array?
    /* for simplicity we use sender's address as the list ID,
      * accepting these consequences: a) if one user wish to maintain
      * several lists with different capacity, he or she must use a
@@ -40,72 +44,70 @@ contract ManagedListERC
   function addList(List list) public;
 
   /* replace list manager's key with the new key */
-  function replaceListKey(List list, address key) public returns(bool);
+  function replaceListIndex(List list, address manager) public returns(bool);
 
+}
+
+contract ValidationContractInstantiation
+{
+    //note: this is implemented inside the issuers contract but the method signature is consistent
+    //We need to instantiate an empty function to use the compiler to call the function in another contract
+    function validate(address attester) public returns(bool) {}
 }
 
 contract IssuerListManager is ManagedListERC
 {
+  //list id is a uint
+  mapping(uint => List[]) lists;
+  address manager; //manager can remove/change/add lists
 
-  mapping(address => List[]) lists;
-
-  function addIssuer(address contractAddress, uint indexOfList) public
+  constructor(address managerOfContract) public
   {
-      lists[msg.sender][indexOfList].issuers.push(contractAddress);
+      manager = managerOfContract;
   }
 
-  function removeIssuer(address contractAddress, List listToRemoveIssuerFrom) public returns(bool)
+  function addIssuer(address issuerContractAddress, uint list_id, uint indexOfList) public
   {
-      for(uint i = 0; i < listToRemoveIssuerFrom.issuers.length; i++)
-      {
-          if(listToRemoveIssuerFrom.issuers[i] == contractAddress)
+      require(msg.sender == manager);
+      lists[list_id][indexOfList].issuerContracts.push(issuerContractAddress);
+  }
+
+  function removeIssuer(address issuerContractAddress, uint list_id) public returns(bool)
+  {
+       require(msg.sender == manager);
+       for(uint i = 0; i < lists[list_id].length; i++)
+       {
+          for(uint j = 0; j < lists[list_id][i].issuerContracts.length; j++)
           {
-
-          }
-      }
-  }
-
-  function getListIndex(address holder, List listToCheck) returns(uint)
-  {
-      for(uint i = 0; i < lists[holder].length; i++)
-      {
-          //if(lists[holder][i] == listToCheck) return i;
-      }
-      throw;
-  }
-
-  //TODO unclear
-  function getIssuerByKey(address issuer, address key_id_to_validate) public returns (address)
-  {
-      for(uint i = 0; i < lists[key_id_to_validate].length; i++)
-      {
-          for(uint j = 0; j < lists[key_id_to_validate][i].issuers.length; j++)
-          {
-              if(lists[key_id_to_validate][i].issuers[j] == issuer)
+              if(lists[list_id][i].issuerContracts[j] == issuerContractAddress)
               {
-                 return issuer;
+                  delete lists[list_id][i].issuerContracts[j];
+                  return true;
               }
           }
+       }
+       return false;
+  }
+
+  function getIssuerCorrespondingToAttestationKey(uint list_id, address signingKeyOfAttestation) public returns (address)
+  {
+      List[] storage listsToQuery = lists[list_id];
+      for(uint i = 0; i < listsToQuery.length; i++)
+      {
+          for(uint j = 0; j < listsToQuery[i].issuerContracts.length; j++)
+          {
+               address issuerContract = listsToQuery[i].issuerContracts[j];
+               bool isValid = ValidationContractInstantiation(issuerContract).validate(signingKeyOfAttestation);
+               if(isValid) return issuerContract;
+          }
       }
+      return address(0);
   }
 
-  function addList(List list) public
+  function addList(List list, uint index) public
   {
-      lists[msg.sender].push(list);
-  }
-
-  function replaceListKey(List list, address key) public returns(bool)
-  {
-    //   for(uint i = 0 ; i < lists[msg.sender].length; i++)
-    //   {
-    //       if(lists[msg.sender][i] == list)
-    //       {
-    //           lists[key].push(lists[msg.sender][i]);
-    //           delete lists[msg.sender][i];
-    //           return true;
-    //       }
-    //   }
-    //   return false;
+      require(msg.sender == manager);
+      lists[index].push(list);
   }
 
 }
