fcdb29d19feb98e7abe82949a4178aefa3652801
colourful-land <a@colourful.land>
2018-10-03 17:49:07 +0800

    add a blank readme page at "/" so that developers run it without looking have something to see

--- attester-webflow/src/main/java/identity/WebMvcConfig.java
+++ attester-webflow/src/main/java/identity/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -21,6 +22,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+      // provide a README for implementors
+      registry.addViewController("/")
+              .setViewName("readme");
     }
 
     @Bean
