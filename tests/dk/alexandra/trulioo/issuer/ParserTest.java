b394978c404bb9111f7a463efbe2cf68b3530794
Weiwu Zhang <a@colourful.land>
2020-10-07 18:07:37 +1100

    renaming attestation class to attestor. reason: they are not attestations, they return attestations

--- tests/dk/alexandra/trulioo/issuer/ParserTest.java
+++ tests/dk/alexandra/trulioo/issuer/ParserTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
-public class TestParser {
+public class ParserTest {
 
   @Test
   public void testSunshine() throws Exception {
