26b04c87e9b8be57147f7feb7fbef430a20d38fb
Tore Frederiksen <jot2re@gmail.com>
2020-12-14 17:08:35 +0100

    Added tests for magic link for ticket

--- src/test/java/com/alphawallet/attestation/TestURL.java
+++ src/test/java/com/alphawallet/attestation/TestURL.java
@@ -10,6 +10,9 @@ import com.alphawallet.attestation.cheque.ChequeDecoder;
 import com.alphawallet.attestation.core.AttestationCrypto;
 import com.alphawallet.attestation.core.SignatureUtility;
 import com.alphawallet.attestation.core.URLUtility;
+import com.alphawallet.attestation.ticket.Ticket;
+import com.alphawallet.attestation.ticket.Ticket.TicketClass;
+import com.alphawallet.attestation.ticket.TicketDecoder;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.SecureRandom;
@@ -34,7 +37,7 @@ public class TestURL {
   }
 
   @Test
-  public void testSunshine() throws IOException  {
+  public void testChequeSunshine() throws IOException  {
     BigInteger senderSecret = new BigInteger("112");
     Cheque cheque = new Cheque("test@test.ts", AttestationType.EMAIL, 1000, 3600000, senderKeys, senderSecret);
 
@@ -54,12 +57,48 @@ public class TestURL {
   }
 
   @Test
-  public void testConsistentEncoding() throws IOException {
+  public void testChequeConsistentEncoding() throws IOException {
     BigInteger senderSecret = new BigInteger("112");
     Cheque cheque = new Cheque("test@test.ts", AttestationType.EMAIL, 1000, 3600000, senderKeys, senderSecret);
     String url = URLUtility.encodeData(cheque.getDerEncoding());
     Cheque newCheque =  (new ChequeDecoder()).decode(URLUtility.decodeData(url));
     String newUrl = URLUtility.encodeData(newCheque.getDerEncoding());
     assertEquals(url, newUrl);
+  }
+
+  @Test
+  public void testTicketSunshine() throws IOException  {
+    BigInteger ticketID = new BigInteger("417541561854");
+    TicketClass ticketClass = TicketClass.REGULAR;
+    BigInteger senderSecret = new BigInteger("45845870684");
+    Ticket ticket = new Ticket("mah@mah.com", ticketID, ticketClass, 6, senderKeys, senderSecret);
+
+    byte[] senderPublicKey = SubjectPublicKeyInfoFactory.createSubjectPublicKeyInfo(senderKeys.getPublic()).getPublicKeyData().getEncoded();
+    String url = URLUtility.encodeList(Arrays.asList(ticket.getDerEncoding(), senderPublicKey));
+
+    List<byte[]> decoded = URLUtility.decodeList(url);
+    Ticket newTicket = (new TicketDecoder(senderKeys.getPublic())).decode(decoded.get(0));
+    assertTrue(newTicket.verify());
+    assertTrue(newTicket.checkValidity());
+    assertArrayEquals(ticket.getDerEncoding(), newTicket.getDerEncoding());
+
+    AsymmetricKeyParameter newIssuerPublicKey = SignatureUtility.restoreKey(decoded.get(1));
+    Ticket otherConstructorTicket = new Ticket(newTicket.getTicketId(), newTicket.getTicketClass(), newTicket.getConferenceId(),
+        newTicket.getRiddle(), newTicket.getSignature(), newIssuerPublicKey);
+    assertArrayEquals(ticket.getDerEncoding(), otherConstructorTicket.getDerEncoding());
+  }
+
+  @Test
+  public void testTicketConsistentEncoding() throws IOException {
+    BigInteger ticketID = new BigInteger("14840860468475837258758376");
+    TicketClass ticketClass = TicketClass.VIP;
+    BigInteger senderSecret = new BigInteger("186416");
+    Ticket ticket = new Ticket("ticket@test.ts", ticketID, ticketClass, 6, senderKeys, senderSecret);
+    String url = URLUtility.encodeData(ticket.getDerEncoding());
+    Ticket newTicket =  (new TicketDecoder(senderKeys.getPublic())).decode(URLUtility.decodeData(url));
+    String newUrl = URLUtility.encodeData(newTicket.getDerEncoding());
+    assertEquals(url, newUrl);
+    /*** PRINT URL ***/
+    System.out.println(url);
   }
 }
