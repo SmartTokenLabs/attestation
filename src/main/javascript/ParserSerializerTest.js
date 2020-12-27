3cadbbcde9c2113ce7afbb98e4d2faf2e016cb4b
Weiwu Zhang <a@colourful.land>
2020-12-28 02:16:59 +1100

    name the files so other classes can be added in

--- src/main/javascript/ParserSerializerTest.js
+++ src/main/javascript/ParserSerializerTest.js
@@ -1,8 +1,9 @@
 
 import { fromBER } from "asn1js";
-import SignedDevconTicket from "./SignedDevonTicket.js"
 import { readFileSync } from 'fs';
 
+// testing SignedDevconTicket
+import SignedDevconTicket from "./SignedDevonTicket.js"
 const der = readFileSync('build/test-results/signed-devcon-ticket.der')
 const ans1 = fromBER(new Uint8Array(der).buffer);
 const ticket = new SignedDevconTicket({
@@ -10,6 +11,8 @@ const ticket = new SignedDevconTicket({
 });
 const { devconId, ticketClass, ticketId, riddle } = ticket;
 const text = `devconId = ${devconId}, ticketId = ${ticketId}, ticketClass = ${ticketClass}, riddle = ${riddle}`;
-
 console.log(text);
+
+// Go on and test every other Parser-Serializer
+
 
