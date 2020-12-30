68c3e748bba97a7d0af4da64d59c39e5e2873333
Weiwu Zhang <a@colourful.land>
2020-12-30 21:33:14 +1100

    Nodejs in gradle configured to use Bhara's code (#62)
    * making the TestTicket process output a test file for JavaScript to work on
    * adding Bharah's code to work with the test cases
    * added testJavaScript task to build.gradle

---
+++ src/test/javascript/ParserSerializerTest.js
@@ -0,0 +1,18 @@
+
+import { fromBER } from "asn1js";
+import { readFileSync } from 'fs';
+
+// testing SignedDevconTicket
+import SignedDevconTicket from "../../main/javascript/SignedDevonTicket.js"
+const der = readFileSync('build/test-results/signed-devcon-ticket.der')
+const ans1 = fromBER(new Uint8Array(der).buffer);
+const ticket = new SignedDevconTicket({
+    schema: ans1.result,
+});
+const { devconId, ticketClass, ticketId, riddle } = ticket;
+const text = `devconId = ${devconId}, ticketId = ${ticketId}, ticketClass = ${ticketClass}, riddle = ${riddle}`;
+console.log(text);
+
+// Go on and test every other Parser-Serializer
+
+
