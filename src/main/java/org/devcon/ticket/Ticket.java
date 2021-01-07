06a9fa4862c5c638f44b2b3fcb20af8a6f9f7455
AW-STJ <sunil.tom.jose@alphawallet.com>
2021-01-08 10:51:25 +1100

    Removed ticket.devcon.org domain name

--- src/main/java/org/devcon/ticket/Ticket.java
+++ src/main/java/org/devcon/ticket/Ticket.java
@@ -28,7 +28,7 @@ public class Ticket implements Attestable {
   private final byte[] commitment;
   private final AlgorithmIdentifier algorithm;
   private final byte[] signature;
-  public static final String magicLinkURLPrefix = "https://ticket.devcon.org/";
+  public static final String magicLinkURLPrefix = "";
   private final AsymmetricKeyParameter publicKey;
   private final byte[] encoded;
 
