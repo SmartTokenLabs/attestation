af10c3dc33d76901c75279d9d75b03b7ad1d6840
James Brown <soylentblue@hotmail.co.uk>
2021-02-04 13:43:14 +1100

    Update contract with correct curve params and proof rejection sampling

--- src/main/java/com/alphawallet/attestation/demo/SmartContract.java
+++ src/main/java/com/alphawallet/attestation/demo/SmartContract.java
@@ -2,12 +2,15 @@ package com.alphawallet.attestation.demo;
 
 import static org.web3j.protocol.core.methods.request.Transaction.createEthCallTransaction;
 
+import com.alphawallet.attestation.FullProofOfExponent;
 import com.alphawallet.attestation.ProofOfExponent;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import com.alphawallet.token.tools.Numeric;
 import okhttp3.OkHttpClient;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
@@ -22,12 +25,18 @@ import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.http.HttpService;
 
 public class SmartContract {
-  private static final String ATTESTATION_CHECKING_CONTRACT = "0xeF3638178D7E775D1B6e5E41c52509a2302582Be";
+  private static final String ATTESTATION_CHECKING_CONTRACT = "0xBfF9E858796Bc8443dd1026D14Ae018EfBE87aD5";
   private static final String ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
   public boolean verifyEqualityProof(byte[] com1, byte[] com2, ProofOfExponent pok) throws Exception
   {
     Function function = verifyEncoding(com1, com2, pok.getDerEncoding());
+    return callFunction(function);
+  }
+
+  public boolean usageProofOfExponent(FullProofOfExponent exp)
+  {
+    Function function = checkEncoding(exp.getDerEncoding());
     return callFunction(function);
   }
 
@@ -85,6 +94,13 @@ public class SmartContract {
       e.printStackTrace();
       return null;
     }
+  }
+
+  private static Function checkEncoding(byte[] encoding) {
+    return new Function(
+            "decodeAttestation",
+            Collections.singletonList(new DynamicBytes(encoding)),
+            Collections.singletonList(new TypeReference<Bool>() {}));
   }
 
   private static Function verifyEncoding(byte[] com1, byte[] com2, byte[] encoding) {
