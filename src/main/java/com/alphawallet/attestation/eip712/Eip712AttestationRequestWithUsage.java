a4a0b8653525e7c83905f9acb93b270de66cd17e
Tore Frederiksen <jot2re@gmail.com>
2021-05-07 16:49:07 +0200

    Minor correction

--- src/main/java/com/alphawallet/attestation/eip712/Eip712AttestationRequestWithUsage.java
+++ src/main/java/com/alphawallet/attestation/eip712/Eip712AttestationRequestWithUsage.java
@@ -1,5 +1,8 @@
 package com.alphawallet.attestation.eip712;
 
+import static com.alphawallet.attestation.eip712.Timestamp.DEFAULT_TIME_LIMIT_MS;
+import static com.alphawallet.attestation.eip712.Timestamp.DEFAULT_TOKEN_TIME_LIMIT;
+
 import com.alphawallet.attestation.AttestationRequestWithUsage;
 import com.alphawallet.attestation.FullProofOfExponent;
 import com.alphawallet.attestation.IdentifierAttestation.AttestationType;
@@ -21,9 +24,6 @@ import org.tokenscript.eip712.JsonEncodable;
 public class Eip712AttestationRequestWithUsage extends Eip712Validator implements JsonEncodable,
     Verifiable, Validateable, TokenValidateable {
   private static final Logger logger = LogManager.getLogger(Eip712AttestationRequestWithUsage.class);
-
-  public static final long DEFAULT_TOKEN_TIME_LIMIT = Timestamp.DEFAULT_TOKEN_TIME_LIMIT;
-  public static final long DEFAULT_TIME_LIMIT_MS = Timestamp.DEFAULT_TIME_LIMIT_MS;
 
   private final long maxTokenValidityInMs;
   private final long acceptableTimeLimit;
