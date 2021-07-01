80b4fd91d6154bb28f7878c04df2597db192e6da
Weiwu Zhang <a@colourful.land>
2021-07-01 23:05:49 +1000

    fixing 1 out of 4 test-case failures

--- src/test/java/com/alphawallet/attestation/eip712/TestAttestationRequestEip712.java
+++ src/test/java/com/alphawallet/attestation/eip712/TestAttestationRequestEip712.java
@@ -15,6 +15,7 @@ import com.alphawallet.attestation.core.URLUtility;
 import com.alphawallet.attestation.eip712.Eip712AttestationRequestEncoder.AttestationRequestInternalData;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Objects;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.ECKeyParameters;
@@ -97,8 +98,9 @@ public class TestAttestationRequestEip712 {
     assertArrayEquals(request.getPok().getDerEncoding(), newRequest.getPok().getDerEncoding());
     assertEquals(request.getJsonEncoding(), newRequest.getJsonEncoding());
     assertEquals(request.getType(), newRequest.getType());
-    assertEquals( ((ECKeyParameters) request.getUserPublicKey()).getParameters(),
-        ((ECKeyParameters) newRequest.getUserPublicKey()).getParameters());
+    ECKeyParameters requestPubKey = (ECKeyParameters) request.getUserPublicKey();
+    ECKeyParameters newRequestPubKey = ((ECKeyParameters) newRequest.getUserPublicKey());
+    assertTrue(Objects.deepEquals(requestPubKey.getParameters(), newRequestPubKey.getParameters()));
   }
 
   @Test
