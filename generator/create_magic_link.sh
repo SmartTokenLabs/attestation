464e7fc565f9e221e5efb9b617c2b47d41f36711
Oleh Hryb <oleg.grib.ua@gmail.com>
2022-04-12 03:59:32 +0100

    removed warnings

--- generator/create_magic_link.sh
+++ generator/create_magic_link.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ../build/libs/attestation-0.3.17-all.jar org.devcon.ticket.Issuer ./key.pem email@email.com 111 222 333 2>/dev/null > ./magiclink.txt 
+java -cp ../build/libs/attestation-0.3.17-all.jar org.devcon.ticket.Issuer ./key.pem email@email.com 111 222 333 | grep -v org.tokenscript.attestation.core.SignatureUtility > ./magiclink.txt 
