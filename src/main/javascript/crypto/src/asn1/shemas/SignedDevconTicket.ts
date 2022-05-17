9e7919115755f85feab99e58d93bbaa6052a45f4
micwallace <micwallace@gmx.com>
2022-05-17 18:20:03 +1000

    Fix typo in ticket schema
    Fixes typo in ticket schema for fields ticketIdNumber & ticketIdString

--- src/main/javascript/crypto/src/asn1/shemas/SignedDevconTicket.ts
+++ src/main/javascript/crypto/src/asn1/shemas/SignedDevconTicket.ts
@@ -10,8 +10,8 @@ import { ValidityValue, AlgorithmIdentifierASN} from "./AuthenticationFramework"
 export class DevconTicket {
     @AsnProp({ type: AsnPropTypes.Utf8String }) public devconId: string;
     // @AsnProp({ type: StringOrInteger }) public ticketId: StringOrInteger;
-    @AsnProp({ type: AsnPropTypes.Integer, optional: true}) public tiketIdNumber?: BigInt;
-    @AsnProp({ type: AsnPropTypes.Utf8String, optional: true }) public tiketIdString?: string;
+    @AsnProp({ type: AsnPropTypes.Integer, optional: true}) public ticketIdNumber?: BigInt;
+    @AsnProp({ type: AsnPropTypes.Utf8String, optional: true }) public ticketIdString?: string;
     @AsnProp({ type: AsnPropTypes.Integer }) public ticketClass: number;
     @AsnProp({ type: AsnPropTypes.OctetString, optional: true }) public commitment?: Uint8Array;
 }
