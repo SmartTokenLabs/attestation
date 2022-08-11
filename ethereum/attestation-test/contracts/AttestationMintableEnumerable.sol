875535891117a4ed72c91782cfe71cfbeebc1532
James Brown <soylentblue@hotmail.co.uk>
2022-08-11 15:48:12 +0100

    update contracts to use standard ERC721 and in-line library to reduce gas cost. Also update tests in line with these changes

--- ethereum/attestation-test/contracts/AttestationMintableEnumerable.sol
+++ ethereum/attestation-test/contracts/AttestationMintableEnumerable.sol
@@ -11,14 +11,14 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
-import "./interface/IVerifyTicket.sol";
+import "./library/VerifyAttestation.sol";
 
 /**
  * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
  * {ERC721Enumerable}.
  */
-contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
+contract AttestationMintableEnumerable is ERC721Enumerable, Ownable, VerifyAttestation {
     using Address for address;
     using Strings for uint256;
     
@@ -29,7 +29,6 @@ contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
     
     address _attestorKey;
     address _issuerKey;
-    address _verificationAddress;
    
     event GenerateTokenId(address indexed addr, uint256 indexed id);
     event GenerateCommitment(address indexed addr, uint256 indexed id);
@@ -37,19 +36,12 @@ contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
     /**
      * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
      */
-    constructor (address verificationAddress, address attestorKey, address issuerKey) ERC721("Proof of Ticket","TICKET") Ownable(){
-        _verificationAddress = verificationAddress;
+    constructor (address attestorKey, address issuerKey) ERC721("Proof of Ticket","TICKET") Ownable(){
         _attestorKey = attestorKey;
         _issuerKey = issuerKey;
     }
 
-    function updateVericationAddress(address newVerifyAddress) public onlyOwner
-    {
-        _verificationAddress = newVerifyAddress;
-    }
-
-    function updateAttestationKeys(address newattestorKey, address newIssuerKey) public onlyOwner
-    {
+    function updateAttestationKeys(address newattestorKey, address newIssuerKey) public onlyOwner {
         _attestorKey = newattestorKey;
         _issuerKey = newIssuerKey;
     }
@@ -66,8 +58,7 @@ contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
         address subject;
         bytes memory tokenBytes;
         bool timeStampValid;
-        IVerifyTicket verifier = IVerifyTicket(_verificationAddress);
-        (subject, tokenBytes,, timeStampValid) = verifier.verifyTicketAttestation(attestation, _attestorKey, _issuerKey);
+        (subject, tokenBytes,, timeStampValid) = verifyTicketAttestation(attestation, _attestorKey, _issuerKey);
         tokenId = bytesToUint(tokenBytes);
         require(subject != address(0) && tokenId != 0 && timeStampValid, "Attestation not valid");
         _mint(subject, tokenId);
@@ -78,30 +69,6 @@ contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
         _burn(tokenId);
     }
     
-    //Truncates if input is greater than 32 bytes; we only handle 32 byte values.
-    function bytesToUint(bytes memory b) private pure returns (uint256 conv)
-    {
-        if (b.length < 0x20) //if b is less than 32 bytes we need to pad to get correct value
-        {
-            bytes memory b2 = new bytes(32);
-            uint startCopy = 0x20 + 0x20 - b.length;
-            assembly
-            {
-                let bcc := add(b, 0x20)
-                let bbc := add(b2, startCopy)
-                mstore(bbc, mload(bcc))
-                conv := mload(add(b2, 32))
-            }
-        }
-        else
-        {
-            assembly
-            {
-                conv := mload(add(b, 32))
-            }
-        }
-    }
-
     function contractAddress() internal view returns (string memory) {
         return Strings.toHexString(uint160(address(this)), 20);
     }
@@ -110,5 +77,4 @@ contract AttestationMintableEnumerable is ERC721Enumerable, Ownable {
     {
         selfdestruct(payable(owner()));
     }
-
 }
